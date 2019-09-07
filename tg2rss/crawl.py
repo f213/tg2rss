9283f255daa7e3c114b6a598f4db4d1d186b284c
Fedor Borshev <fedor@borshev.com>
2019-09-07 19:25:42 +0300

    Some 12-factorness

--- tg2rss/crawl.py
+++ tg2rss/crawl.py
@@ -1,19 +1,24 @@
+from envparse import env
 from scrapy.crawler import CrawlerProcess
 
 from spiders.pmdaily import PmdailySpider
 
+env.read_envfile()
+
+settings = dict(  # settings are here to fail at import time
+    ITEM_PIPELINES={
+        'scrapy_rss.pipelines.RssExportPipeline': 900,
+    },
+    FEED_FILE=env('FEED_FILE', cast=str),
+    FEED_TITLE=env('FEED_TITLE', cast=str),
+    FEED_DESCRIPTION=env('FEED_DESCRIPTION', cast=str),
+    FEED_LINK=env('FEED_LINK', cast=str),
+    LOG_LEVEL=env('LOG_LEVEL', cast=str, default='ERROR'),
+)
+
 
 def crawl():
-    crawler = CrawlerProcess(dict(
-        ITEM_PIPELINES={
-            'scrapy_rss.pipelines.RssExportPipeline': 900,
-        },
-        FEED_FILE='/tmp/feed.rss',
-        FEED_TITLE='t.me/@pmdaily RSS feed',
-        FEED_DESCRIPTION='',
-        FEED_LINK='https://borshev.com/rss/tg/',
-    ))
-
+    crawler = CrawlerProcess(settings)
     crawler.crawl(PmdailySpider)
     crawler.start()
 
